04e79fa1ec3f15024ce6b2a18885423f2134458a
= <=>
2023-04-10 15:15:16 +0900

    4/6 수업 완료

---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Todo extends React.Component{
+  render(){
+    return(
+      <div className="Todo">
+        <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
+        <label for="todo0">todo 컴포넌트 만들기</label>
+        {/*안녕 */}
+        {/*  */}
+      </div>
+    )
+  }
+}
+
+export default Todo;
