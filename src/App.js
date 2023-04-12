92f2dae6ca5872fb6a0a88ce5a260dfcf5b07352
= <=>
2023-04-12 13:15:11 +0900

    4/12 수업 종료

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import Todo from './Todo'
+import Todo from './Todo';
+import { Paper, List ,Container} from "@material-ui/core";
 import './App.css';
-
+import AddTodo from './AddTodo';
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      item:{ id:0, title:"hello", done : true}
+      items:[
+        { id:0, title:"hello1", done : true },
+        { id:1, title:"hello2", done : false },
+        { id:2, title:"hello3", done : false }]
     }
   }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id='ID-'+thisItems.length;
+    item.done=false;
+    thisItems.push(item);
+    this.setState({items:thisItems});
+    console.log("items : ",this.state.items);
+  }
   render(){
-    return(
-      <div className="App">
-        <Todo item={this.state.item}/>
-        <Todo/>
-      </div>
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{margin:16}}>
+        <List>
+          {this.state.items.map((item,idx)=>(<Todo item={item} key={item.id}/>))}
+        </List>
+      </Paper>
     )
+    return (
+    <div className='App'>
+    <Container maxWidth="md">
+        <AddTodo add={this.add}/>
+        <div className='TodoList'>{todoItems}</div>
+      </Container>
+    </div>
+      )
   }
 }
 // function App() {
--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,29 @@
 import React from "react";
-
+import { ListItem,ListItemText,InputBase,Checkbox } from "@material-ui/core";
 class Todo extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={ item: props.item };
+  }
   render(){
+    const item=this.state.item;
     return(
-      <div className="Todo">
-        <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
-        <label for="todo0">todo 컴포넌트 만들기</label>
-        {/*안녕 */}
-        {/*  */}
-      </div>
+      <ListItem>
+        <Checkbox checked={item.done}/>
+        
+        <ListItemText>
+          <InputBase 
+          inputProps={{"aria-label" : "naked"}} 
+          type="text" 
+          id={item.id} 
+          name={item.id} 
+          value={item.title} 
+          multiline={true} 
+          fullWidth={true}/>
+        </ListItemText>
+
+      </ListItem>
     )
   }
 }
